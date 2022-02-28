8d55f2dd544c32227d6cae3d3e5f15b0791997d6
Re-Ki <rekireki788@gmail.com>
2022-02-28 10:01:38 +0630

    DSA

--- CPP/linkedlist append/cmake-build-debug/cmake_install.cmake
+++ CPP/linkedlist append/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Re-Ki/CPP/linkedlist
+# Install script for directory: C:/Re-Ki/CPP/linkedlist append
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Re-Ki/CPP/linkedlist/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Re-Ki/CPP/linkedlist append/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- CPP/linkedlist append/main.cpp
+++ CPP/linkedlist append/main.cpp
@@ -1,6 +1,97 @@
-#include <iostream>
+#include "iostream"
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+class Node{
+public:
+    int data;
+    Node*next;
+};
+
+void push(Node** head_ref , int new_data){
+
+    Node* new_node = new Node();
+    new_node -> data = new_data;
+
+    new_node -> next = (*head_ref);
+    (*head_ref) =  new_node;
+}
+
+void append(Node** head_ref , int new_data){
+
+    Node* new_node = new Node();
+    Node* last = *head_ref;
+
+    new_node -> data = new_data;
+    new_node -> next = NULL;
+
+    if(*head_ref == NULL){
+
+        *head_ref = new_node;
+        return;
+    }
+
+    while (last -> next != NULL){
+
+        last = last ->next;
+    }
+    last -> next = new_node;
+    return;
+}
+
+// int a=NULL; int b = NULL;
+// normal pointer stored address of variable
+// Node -> next -> Node -> next -> Node -> next -> Node -> next -> NULL;
+
+void dt_print(Node* node){
+
+    while(node != NULL){
+
+        cout<<node -> data<<" ";
+        node = node -> next;
+    }
+}
+
+int main(){
+
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head -> data = 10;
+    head -> next = second;
+
+    second -> data = 20;
+    second -> next = third;
+
+    third -> data = 30;
+    third -> next = NULL;
+
+    cout<<"Before push"<<endl;
+    dt_print(head);
+    cout<<endl;
+
+    push(&head,100);
+    cout<<"After push"<<endl;
+    dt_print(head);
+    cout<<endl;
+
+    push(&head,200);
+    cout<<"After push"<<endl;
+    dt_print(head);
+    cout<<endl;
+
+    append(&head , 111);
+    cout<<"After append"<<endl;
+    dt_print(head);
+    cout<<endl;
+
+    append(&head , 222);
+    cout<<"After append2"<<endl;
+    dt_print(head);
+
     return 0;
 }
